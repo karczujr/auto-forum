a8f5a55dd663ea1dafe7eecda03d4ca73749538b
karczujr <125811152+karczujr@users.noreply.github.com>
2023-03-19 18:21:28 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    validateInputs();
+})
